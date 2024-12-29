f1e4144eabddec24cc64adb189327234bf327096
Minh Le <100dayscode.minh@gmail.com>
2024-12-29 21:03:43 +1100

    clean up backend code

--- mysite/chatbot/ai_components/ai_chatbot.py
+++ mysite/chatbot/ai_components/ai_chatbot.py
@@ -6,7 +6,6 @@ from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
 
 
-
 class AIChatBot:
     def __init__(self):
         self.llm = get_openai_model()
@@ -17,24 +16,7 @@ class AIChatBot:
 
         self.chat_history = {}
 
-    def get_chat_history(self, session_id: str):
-
-        if session_id not in self.chat_history:
-            self.chat_history[session_id] = ChatMessageHistory()
-        return self.chat_history[session_id]
-
-    @staticmethod
-    def get_session_id():
-        return str(uuid.uuid4())
-
-    def get_llm_response(self, prompt: str, user_session_id: str) -> str:
-
-        # prompt_template = ChatPromptTemplate.from_template(self.system_template)
-        # chain = prompt_template | self.llm
-        # response = chain.invoke({"input": prompt})
-        # return response.content
-
-        chat_prompt = ChatPromptTemplate.from_messages(
+        self.chat_prompt = ChatPromptTemplate.from_messages(
             [
                 (
                     "system",
@@ -44,10 +26,23 @@ class AIChatBot:
                 ("human", "{input}"),
             ]
         )
-        chain = chat_prompt | self.llm
+
+        self.chain = self.chat_prompt | self.llm
+
+    @staticmethod
+    def get_session_id():
+        return str(uuid.uuid4())
+
+    def get_chat_history(self, session_id: str):
+
+        if session_id not in self.chat_history:
+            self.chat_history[session_id] = ChatMessageHistory()
+        return self.chat_history[session_id]
+
+    def get_llm_response(self, prompt: str, user_session_id: str) -> str:
 
         chatbot_with_memory = RunnableWithMessageHistory(
-            chain,
+            self.chain,
             # pass the session into the get chat history and retrieve list of chat message
             lambda session_id: self.get_chat_history(session_id),
             input_messages_key =  "input",
